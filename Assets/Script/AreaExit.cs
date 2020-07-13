a8aff9e69e7d1dc67a2f8a1e58f9835848cbc93a
unknown <Agm2440@email.vccs.edu>
2020-07-13 16:21:57 -0400

    ArnFirstCommit

---
+++ Assets/Script/AreaExit.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AreaExit : MonoBehaviour
+{
+    public string areaToLoad;
+    public string areaTransitionName;
+    public AreaEntrance theEntrance;
+    public float waitToLoad = 1f;
+    private bool shouldLoadAfterFade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theEntrance.transitionName = areaTransitionName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fadeScreen();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        { 
+            print("loading");
+            shouldLoadAfterFade = true;
+            GameManager.gameManagerInstance.fadingBetweenAreas = true;
+            UIFade.imageInstance.FadeToBlack();
+            Player.playerInstance.areaToTransitionName = areaTransitionName;
+        }
+    }
+
+    void fadeScreen()
+    {
+        if (shouldLoadAfterFade)
+        {
+            waitToLoad -= Time.deltaTime;
+            if (waitToLoad <= 0)
+            {
+                shouldLoadAfterFade = false;
+                SceneManager.LoadScene(areaToLoad);
+            }
+
+        }
+    }
+}
---
+++ Assets/Script/DialogActivator.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogActivator : MonoBehaviour
+{
+    public string[] characterLines;
+
+    public GameObject shopButtons;
+    
+    Characters chars;
+
+    int timeToOpen;
+
+    int waitToOpen = 1;
+
+    private bool canActivate;
+
+    public bool isTalking;
+
+    public bool isPerson = true;
+
+    private void Update()
+    {
+        displayLines();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        chars = this.GetComponent<Characters>();
+
+        if(other.tag == "Player")
+        {
+            canActivate = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            canActivate = false;
+        }
+    }
+
+    void displayLines()
+    {
+        if (canActivate == true && Input.GetButtonDown("Fire1") && !DialogManager.instance.dialogBox.activeInHierarchy && chars.characterCategory == Characters.CharacterCategory.NPC)
+        {
+            DialogManager.instance.ShowDialog(characterLines, isPerson);
+            shopButtons.SetActive(false);
+
+        }
+        else if(canActivate == true && Input.GetButtonDown("Fire1") && !DialogManager.instance.dialogBox.activeInHierarchy && chars.characterCategory == Characters.CharacterCategory.SHOPKEEPER)
+        {
+            DialogManager.instance.ShowDialog(characterLines, isPerson);
+            shopButtons.SetActive(true);
+        }
+    }
+}
